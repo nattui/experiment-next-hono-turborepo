8940a558cda77fc1b5d1778eeaf2f4d6c5ea2dca
Natt Nguyen <natt.lefevre@gmail.com>
2025-09-21 17:04:44 +0100

    fix: disable fetch cache

--- apps/web/app/test/page.tsx
+++ apps/web/app/test/page.tsx
@@ -1,7 +1,7 @@
 import { API } from "@/utils/url"
 
 export default async function TestPage() {
-  const response = await fetch(API.TEST)
+  const response = await fetch(API.TEST, { cache: "no-store" })
   const data = await response.json()
 
   return <div>{data.data}</div>
