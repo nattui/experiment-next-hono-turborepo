1757233d7571ebab3ca91c21d16edcea6713435e
Natt Nguyen <natt.lefevre@gmail.com>
2025-09-22 08:25:03 -0700

    feat: logger

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,10 +2,13 @@ import { serve } from "@hono/node-server"
 import { Hono } from "hono"
 import { auth } from "./routes/auth/index.js"
 import { main } from "./routes/main/index.js"
+import { logger } from "hono/logger"
 
 export const isDevelopment = process.env.NODE_ENV === "development"
 
 const app = new Hono()
+
+app.use(logger())
 
 // Mount route modules
 app.route("/", main)
