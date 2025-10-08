66bbfc1f23bec9f0e34b7ff5effe1379b7f700f8
Natt Nguyen <natt.lefevre@gmail.com>
2025-10-08 09:57:20 -0700

    feat: add test route to app initialization for enhanced functionality and routing clarity

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,11 +1,12 @@
 import { Hono } from "hono"
 import { handlerRoot } from "./routes/main/root.route.js"
+import { handlerTest } from "./routes/main/test.route.js"
 
 // const app = new Hono()
 // app.route("/", routeRoot)
 // app.route("/test", routeTest)
 
-const app = new Hono().get("/", handlerRoot)
+const app = new Hono().get("/", handlerRoot).get("/test", handlerTest)
 
 export default {
   fetch: app.fetch,
