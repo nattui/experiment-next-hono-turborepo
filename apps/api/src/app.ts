9579053a9c234ad9306bf8bbd635550a6448ca13
Natt Nguyen <natt.lefevre@gmail.com>
2025-10-08 10:20:44 -0700

    feat: add authentication routes for signin, signout, signup, and verification to enhance user management functionality

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,4 +1,8 @@
 import { Hono } from "hono"
+import { routeSigninCredential } from "./routes/auth/signin-credential.route.js"
+import { routeSignout } from "./routes/auth/signout.route.js"
+import { routeSignupCredential } from "./routes/auth/signup-credential.route.js"
+import { routeVerify } from "./routes/auth/verify.route.js"
 import { routeRoot } from "./routes/main/root.route.js"
 import { routeTest } from "./routes/main/test.route.js"
 import { routeUsers } from "./routes/main/users.route.js"
@@ -7,6 +11,10 @@ const app = new Hono()
   .route("/", routeRoot)
   .route("/test", routeTest)
   .route("/users", routeUsers)
+  .route("/auth/signin/credential", routeSigninCredential)
+  .route("/auth/signout", routeSignout)
+  .route("/auth/signup/credential", routeSignupCredential)
+  .route("/auth/verify", routeVerify)
 
 export type AppType = typeof app
 
