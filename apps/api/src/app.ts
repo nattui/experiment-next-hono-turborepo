583ece5d0aae007951aa46de5c6e8fb49c6eee52
Natt Nguyen <natt.lefevre@gmail.com>
2025-10-08 09:55:29 -0700

    refactor: update app initialization to use handlerRoot for root route and simplify route structure

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,11 +1,11 @@
 import { Hono } from "hono"
-import { routeRoot } from "./routes/main/root.route.js"
-import { routeTest } from "./routes/main/test.route.js"
+import { handlerRoot } from "./routes/main/root.route.js"
 
-const app = new Hono()
+// const app = new Hono()
+// app.route("/", routeRoot)
+// app.route("/test", routeTest)
 
-app.route("/", routeRoot)
-app.route("/test", routeTest)
+const app = new Hono().get("/", handlerRoot)
 
 export default {
   fetch: app.fetch,
