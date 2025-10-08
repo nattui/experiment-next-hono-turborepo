c81e5721613b38b2d3b35b692f13077238299959
Natt Nguyen <natt.lefevre@gmail.com>
2025-10-08 09:43:06 -0700

    refactor: update TypeScript configuration and comment out authentication and main route handlers for improved clarity and modularity

--- apps/api/src/routes/auth/auth.route.ts
+++ apps/api/src/routes/auth/auth.route.ts
@@ -1,24 +1,24 @@
-import { Hono } from "hono"
-import { verifyMiddleware } from "../../middleware/auth.middleware"
-import {
-  handlerSigninCredential,
-  routeSigninCredential,
-} from "./signin-credential.route"
-import { handlerSignout, routeSignout } from "./signout.route"
-import {
-  handlerSignupCredential,
-  routeSignupCredential,
-} from "./signup-credential.route"
-import { handlerVerify, routeVerify } from "./verify.route"
+// import { Hono } from "hono"
+// import { verifyMiddleware } from "../../middleware/auth.middleware"
+// import {
+//   handlerSigninCredential,
+//   routeSigninCredential,
+// } from "./signin-credential.route"
+// import { handlerSignout, routeSignout } from "./signout.route"
+// import {
+//   handlerSignupCredential,
+//   routeSignupCredential,
+// } from "./signup-credential.route"
+// import { handlerVerify, routeVerify } from "./verify.route"
 
-export const routeAuth = new Hono()
-routeAuth.route("/signin/credential", routeSigninCredential)
-routeAuth.route("/signup/credential", routeSignupCredential)
-routeAuth.route("/signout", routeSignout)
-routeAuth.route("/verify", routeVerify)
+// export const routeAuth = new Hono()
+// routeAuth.route("/signin/credential", routeSigninCredential)
+// routeAuth.route("/signup/credential", routeSignupCredential)
+// routeAuth.route("/signout", routeSignout)
+// routeAuth.route("/verify", routeVerify)
 
-export const routeChainedAuth = new Hono()
-  .get("/verify", verifyMiddleware, handlerVerify)
-  .post("/signin/credential", handlerSigninCredential)
-  .post("/signout", handlerSignout)
-  .post("/signup/credential", handlerSignupCredential)
+// export const routeChainedAuth = new Hono()
+//   .get("/verify", verifyMiddleware, handlerVerify)
+//   .post("/signin/credential", handlerSigninCredential)
+//   .post("/signout", handlerSignout)
+//   .post("/signup/credential", handlerSignupCredential)
--- apps/api/src/routes/main/main.route.ts
+++ apps/api/src/routes/main/main.route.ts
@@ -1,14 +1,14 @@
-import { Hono } from "hono"
-import { handlerRoot } from "./root.route"
-import { handlerTest, routeTest } from "./test.route"
-import { handlerUsers, routeUsers } from "./users.route"
+// import { Hono } from "hono"
+// import { handlerRoot } from "./root.route"
+// import { handlerTest, routeTest } from "./test.route"
+// import { handlerUsers, routeUsers } from "./users.route"
 
-export const routeMain = new Hono()
-// routeMain.route("/", routeRoot)
-routeMain.route("/test", routeTest)
-routeMain.route("/users", routeUsers)
+// export const routeMain = new Hono()
+// // routeMain.route("/", routeRoot)
+// routeMain.route("/test", routeTest)
+// routeMain.route("/users", routeUsers)
 
-export const routeChainedMain = new Hono()
-  .get("/", handlerRoot)
-  .get("/test", handlerTest)
-  .get("/users", handlerUsers)
+// export const routeChainedMain = new Hono()
+//   .get("/", handlerRoot)
+//   .get("/test", handlerTest)
+//   .get("/users", handlerUsers)
