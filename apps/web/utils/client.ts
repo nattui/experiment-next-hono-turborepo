bdbcf968095a61b73993f5c92bf8f2fa5b831a41
Natt Nguyen <natt.lefevre@gmail.com>
2025-10-08 01:30:35 -0700

    refactor: simplify client initialization by removing unnecessary credentials option

--- apps/web/utils/client.ts
+++ apps/web/utils/client.ts
@@ -2,11 +2,7 @@ import type { AppType } from "api"
 import { hc, type InferResponseType } from "hono/client"
 import { BASE_URL } from "@/utils/url"
 
-export const client = hc<AppType>(`${BASE_URL.WEB}/api`, {
-  init: {
-    credentials: "include",
-  },
-})
+export const client = hc<AppType>(`${BASE_URL.WEB}/api`)
 
 export type Client = typeof client
 
