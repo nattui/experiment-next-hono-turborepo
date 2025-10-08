b4a110b7024b05cda277daa043d203ad108260f2
Natt Nguyen <natt.lefevre@gmail.com>
2025-10-08 09:08:46 -0700

    refactor: change routeTest to a local constant and export it for better modularity

--- apps/api/src/routes/test.ts
+++ apps/api/src/routes/test.ts
@@ -1,7 +1,9 @@
 import { Hono } from "hono"
 
-export const routeTest = new Hono()
+const routeTest = new Hono()
 
 routeTest.get("/", (context) => {
   return context.text("Hello Hono!")
 })
+
+export { routeTest }
