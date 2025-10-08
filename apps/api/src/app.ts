2a9328454c405200bfec9dee00449e7e510fd979
Natt Nguyen <natt.lefevre@gmail.com>
2025-10-08 00:09:14 -0700

    test: Vercel 2

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,18 +1,20 @@
 import { Hono } from "hono"
 // import { verifyMiddleware } from "./middleware/auth.middleware"
-import { loggerMiddleware } from "./middleware/logger.middleware"
+// import { loggerMiddleware } from "./middleware/logger.middleware"
 // import { routeSigninCredential } from "./routes/auth/signin-credential.route"
 // import { routeSignout } from "./routes/auth/signout.route"
 // import { routeSignupCredential } from "./routes/auth/signup-credential.route"
 // import { routeVerify } from "./routes/auth/verify.route"
-import { routeRoot } from "./routes/main/root.route"
+// import { routeRoot } from "./routes/main/root.route"
 // import { routeTest } from "./routes/main/test.route"
 // import { routeUsers } from "./routes/main/users.route"
 import type { Account, Profile, User } from "./utils/db/schema/user.schema"
 
 export const isDevelopment = process.env.NODE_ENV === "development"
 
-const app = new Hono().use(loggerMiddleware()).get("/", routeRoot)
+const app = new Hono()
+// .use(loggerMiddleware())
+// .get("/", routeRoot)
 // .get("/test", routeTest)
 // .get("/users", routeUsers)
 // .post("/auth/signin/credential", routeSigninCredential)
