08b8a79276ce591aa0dc9b53958595be07416391
Natt Nguyen <natt.lefevre@gmail.com>
2025-10-08 19:26:27 -0700

    refactor: simplify route definitions in main.route.ts by using route method for better organization

--- apps/api/src/routes/main/main.route.ts
+++ apps/api/src/routes/main/main.route.ts
@@ -3,7 +3,11 @@ import { handlerRoot } from "./root.route.js"
 import { handlerTest } from "./test.route.js"
 import { handlerUsers } from "./users.route.js"
 
+const routeRoot = new Hono().get("/", handlerRoot)
+const routeTest = new Hono().get("/test", handlerTest)
+const routeUsers = new Hono().get("/users", handlerUsers)
+
 export const routeMain = new Hono()
-  .get("/", handlerRoot)
-  .get("/test", handlerTest)
-  .get("/users", handlerUsers)
+  .route("/", routeRoot)
+  .route("/test", routeTest)
+  .route("/users", routeUsers)
