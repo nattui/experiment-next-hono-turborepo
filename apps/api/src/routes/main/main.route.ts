bf6f0bb5b40931a1a8adc528aa2832e2f7c603a3
Natt Nguyen <natt.lefevre@gmail.com>
2025-10-08 19:28:59 -0700

    refactor: streamline route definitions in main.route.ts for improved clarity and maintainability

--- apps/api/src/routes/main/main.route.ts
+++ apps/api/src/routes/main/main.route.ts
@@ -3,11 +3,7 @@ import { handlerRoot } from "./root.route.js"
 import { handlerTest } from "./test.route.js"
 import { handlerUsers } from "./users.route.js"
 
-const routeRoot = new Hono().get("/", handlerRoot)
-const routeTest = new Hono().get("/test", handlerTest)
-const routeUsers = new Hono().get("/users", handlerUsers)
-
 export const routeMain = new Hono()
-  .route("/", routeRoot)
-  .route("/test", routeTest)
-  .route("/users", routeUsers)
+  .get("/", handlerRoot)
+  .get("/test", handlerTest)
+  .get("/users", handlerUsers)
