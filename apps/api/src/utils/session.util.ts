0d18411fa1a82c32f6e61eebb50140488a50f592
Natt Nguyen <natt.lefevre@gmail.com>
2025-10-08 20:44:04 -0700

    refactor: correct expiration time constant for session management to reflect accurate duration

--- apps/api/src/utils/session.util.ts
+++ apps/api/src/utils/session.util.ts
@@ -4,7 +4,7 @@ import { sign, verify } from "hono/jwt"
 import type { JWTPayload } from "hono/utils/jwt/types"
 import { JWT_SECRET } from "./constant.util.js"
 
-export const EXPIRATION_TIME_IN_MILLISECONDS = 31_536_000_000_000 // 1 year
+export const EXPIRATION_TIME_IN_MILLISECONDS = 31_536_000_000 // 1 year
 export const JWT_ALGORITHM = "HS256"
 
 export function deleteSession(context: Context): void {
