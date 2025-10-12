7c9415b32d65eaea4ac2a4b528e05152d778b83e
Natt Nguyen <natt.lefevre@gmail.com>
2025-10-11 22:31:20 -0700

    fix: vercel web deployment

--- apps/web/app/test/page.tsx
+++
@@ -1,8 +0,0 @@
-import { client } from "@/utils/client"
-
-export default async function TestPage() {
-  const response = await client.test.$get()
-  const data = await response.json()
-
-  return <div>{data.data}</div>
-}
