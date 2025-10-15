e9daff5ab5fdf926ebe509321237c6e59e66f388
Natt Nguyen <natt.lefevre@gmail.com>
2025-10-15 12:40:09 -0700

    refactor: clean up TRPC router by removing unused imports and streamlining code structure for better readability

--- apps/trpc/src/routes/router.ts
+++ apps/trpc/src/routes/router.ts
@@ -1,10 +1,7 @@
 import { initTRPC } from "@trpc/server"
 import type { Context } from "hono"
-import { setCookie } from "hono/cookie"
-import { db } from "@/db"
 import { routeTest } from "@/routes/test.route"
 import { routeUsers } from "@/routes/users.route"
-import { USER } from "@/schema/user.schema"
 
 export interface AppContext {
   honoContext: Context
