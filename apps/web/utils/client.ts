f76f17f0e4fe6b2568367a6f56895e42846eb79d
Natt Nguyen <natt.lefevre@gmail.com>
2025-10-15 21:15:49 -0700

    feat: add NODE_ENV variable to .env.example and integrate loggerLink in TRPC client for enhanced development debugging

--- apps/web/utils/client.ts
+++ apps/web/utils/client.ts
@@ -1,7 +1,13 @@
-import { createTRPCProxyClient, httpBatchLink } from "@trpc/client"
+import { createTRPCProxyClient, httpBatchLink, loggerLink } from "@trpc/client"
 import type { AppRouter } from "api"
 import { BASE_URL } from "@/utils/url"
 
 export const client = createTRPCProxyClient<AppRouter>({
-  links: [httpBatchLink({ url: `${BASE_URL.WEB}/api` })],
+  links: [
+    loggerLink({
+      enabled: () =>
+        process.env.NODE_ENV === "development" && typeof window !== "undefined",
+    }),
+    httpBatchLink({ url: `${BASE_URL.WEB}/api` }),
+  ],
 })
