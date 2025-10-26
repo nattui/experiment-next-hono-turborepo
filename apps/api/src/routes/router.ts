9c7807dc214db1cd15d8f7ae6247e25eb1395967
Natt Nguyen <natt.lefevre@gmail.com>
2025-10-26 11:43:24 -0700

    feat: add new test and users routes, reorganizing imports for improved structure in the API

--- apps/api/src/routes/router.ts
+++ apps/api/src/routes/router.ts
@@ -3,8 +3,8 @@ import { signinCredential } from "@/routes/auth/signin-credential.route"
 import { signout } from "@/routes/auth/signout.route"
 import { signupCredential } from "@/routes/auth/signup-credential.route"
 import { verify } from "@/routes/auth/verify.route"
-import { test } from "@/routes/auth/main/test.route"
-import { users } from "@/routes/auth/main/users.route"
+import { test } from "@/routes/main/test.route"
+import { users } from "@/routes/main/users.route"
 
 export const router = {
   auth: {
