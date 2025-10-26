04725d5e75d0c9c8fffa716e08c3730dd059b976
Natt Nguyen <natt.lefevre@gmail.com>
2025-10-26 15:05:29 -0700

    feat: enhance seeding functionality by adding account and profile schemas with refined data generation for improved database population

--- packages/db/src/seed.ts
+++ packages/db/src/seed.ts
@@ -1,13 +1,65 @@
 import { seed } from "drizzle-seed"
 import { client, db } from "@/db"
-import { USER } from "@/schema/user.schema"
+import { ACCOUNT, PROFILE, USER } from "@/schema/user.schema"
 
 async function main() {
   if (!process.env.DATABASE_URL) {
     throw new Error("DATABASE_URL is not set")
   }
 
-  await seed(db, { user: USER }, { count: 10 })
+  await seed(
+    db,
+    { account: ACCOUNT, profile: PROFILE, user: USER },
+    { count: 10 },
+  ).refine((f) => ({
+    account: {
+      columns: {
+        password: f.loremIpsum({ sentencesCount: 1 }),
+        provider: f.valuesFromArray({
+          values: [
+            {
+              values: ["credentials"],
+              weight: 1,
+            },
+            {
+              values: ["github"],
+              weight: 0,
+            },
+            {
+              values: ["google"],
+              weight: 0,
+            },
+          ],
+        }),
+      },
+    },
+    profile: {
+      columns: {
+        image: f.string({
+          isUnique: false,
+        }),
+      },
+    },
+    user: {
+      columns: {
+        email: f.email(),
+        emailVerified: f.boolean(),
+        name: f.fullName(),
+        role: f.valuesFromArray({
+          values: [
+            {
+              values: ["admin"],
+              weight: 0,
+            },
+            {
+              values: ["user"],
+              weight: 1,
+            },
+          ],
+        }),
+      },
+    },
+  }))
 
   await client.end()
 }
